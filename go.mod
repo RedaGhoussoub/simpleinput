02245ff194aa5c79b4a894102e6d0d2a4eb4b306
reda.ghoussoub@gmail.com <reda.ghoussoub@gmail.com>
2022-07-27 20:50:00 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/redaghoussoub/simpleinput
+
+go 1.18
---
+++ helper/helper.go
@@ -0,0 +1,23 @@
+package helper
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadIntFromKeyboard() int {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	value, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return value
+}
