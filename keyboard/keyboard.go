aee18cce6517837cbf28e6a0c3e70fc11ce12ac5
reda.ghoussoub@gmail.com <reda.ghoussoub@gmail.com>
2022-07-27 21:36:18 -0400

    refactored methods

--- keyboard/keyboard.go
+++ keyboard/keyboard.go
@@ -2,32 +2,43 @@ package keyboard
 
 import (
 	"bufio"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func ReadInt() int {
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+func ReadInt() (int, bool) {
+	if input, ok := read(); ok {
+		value, err := strconv.Atoi(input)
+		if err != nil {
+			return 0, false
+		}
+		return value, true
 	}
-	input = strings.TrimSpace(input)
-	value, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return value
+	return 0, false
 }
 
-func ReadString() string {
+func ReadFloat() (float64, bool) {
+	if input, ok := read(); ok {
+		value, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			return 0, false
+		}
+		return value, true
+	}
+	return 0, false
+}
+
+func ReadString() (string, bool) {
+	return read()
+}
+
+func read() (string, bool) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		return "", false
 	}
-	value := strings.TrimSpace(input)
-	return value
+	input = strings.TrimSpace(input)
+	return input, true
 }
